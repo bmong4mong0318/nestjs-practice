24633dc08a6d48c7bfe1df30e7fde6d53cc98573
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-03-12 17:05:03 +0900

    feat: Configure typeORM

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,19 @@
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {BoardStatus} from "./boards.model";
+
+@Entity()
+export class Board extends BaseEntity {
+
+    // 기본키
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}
