6fe1f6a447d61d39508dbc2f98c1066ade366de3
yundaehyeok <bmong4mong0318@gmail.com>
2023-03-26 18:07:32 +0900

    Update project

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -9,11 +9,19 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-	constructor(private usersService: UsersService) {}
+	constructor(
+		private usersService: UsersService,
+		private authService: AuthService,
+	) {}
 
 	@Post('/signup')
 	createUser(@Body() body: CreateUserDto) {
-		this.usersService.create(body.email, body.password);
+		return this.authService.signup(body.email, body.password);
+	}
+
+	@Post('/signin')
+	signin(@Body() body: CreateUserDto) {
+		return this.authService.signin(body.email, body.password);
 	}
 
 	@Get('/:id')
